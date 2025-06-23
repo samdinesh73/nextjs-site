6241132ee326104646b20cd20f9dc8f1a96565d8
rock102023 <157478747+rock102023@users.noreply.github.com>
2025-06-23 11:09:12 +0530

    testing purpose

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
-      <Button>Click me</Button>
+      <Button>Clicks me</Button>
     </div>
     <Accordion
       type="single"
@@ -160,6 +160,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <h1>Changed</h1>
     </div>
   );
 }
